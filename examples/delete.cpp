4dd2a75c194192ea62af796d224bab1bd647bf6c
Dino Jose Almirall <almirall.dino@gmail.com>
2023-07-16 20:08:52 +0200

    added delete example

--- examples/delete.cpp
+++ examples/delete.cpp
@@ -8,7 +8,16 @@ int main()
 
     if (config.configOpen)
     {
-    }
+        if (config.deleteSection("print"))
+            std::cout << "Deleted section: print" << std::endl;
+        else
+            std::cout << "Could not delete section: print" << std::endl;
+
+        if (config.deleteSetting("server", "ports"))
+            std::cout << "Deleted section: server , setting: ports" << std::endl;
+        else
+            std::cout << "Could not delete section: server , setting: ports" << std::endl;
+        }
     else
     {
         std::cerr << "Failed to open config file: " << configPath << std::endl;
